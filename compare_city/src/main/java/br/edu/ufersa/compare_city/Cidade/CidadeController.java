ee13f651e1f6eeee1fec7c6e719a06224ceb01c2
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-27 17:05:01 -0300

    FEAT: Ler arquivo JSON e transformar em LISTA de objeto Cidade

--- compare_city/src/main/java/br/edu/ufersa/compare_city/Cidade/CidadeController.java
+++ compare_city/src/main/java/br/edu/ufersa/compare_city/Cidade/CidadeController.java
@@ -1,8 +1,14 @@
 package br.edu.ufersa.compare_city.Cidade;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class CidadeController {
@@ -16,4 +22,19 @@ public class CidadeController {
         this.cidadesService = cidadesService;
     }
 
+    public static void main(String[] args) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                // Ler o arquivo JSON e converter para uma lista de objetos Cidade
+                List<Cidade> cidades = objectMapper.readValue(new File("dados.json"), objectMapper.getTypeFactory().constructCollectionType(List.class, Cidade.class));
+
+                // Exibir as cidades
+                for (Cidade cidade : cidades) {
+                    System.out.println(cidade);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 }
