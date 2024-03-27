084a9546d3d0a19c33caf292c16dd1f8809eece3
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-26 22:16:05 -0300

    UPDATE: Controllador possui uma rota

--- compare_city/src/main/java/br/edu/ufersa/compare_city/Comparador/ComparadorController.java
+++ compare_city/src/main/java/br/edu/ufersa/compare_city/Comparador/ComparadorController.java
@@ -1,5 +1,18 @@
 package br.edu.ufersa.compare_city.Comparador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ComparadorController {
-    
+    @GetMapping("/cidades/comparar")
+    @ResponseBody
+    public String home(@RequestParam(name = "estado1") String estado1,
+            @RequestParam(name = "cidade1") String cidade1,
+            @RequestParam(name = "estado2") String estado2,
+            @RequestParam(name = "cidade2") String cidade2) {
+        return "Comparando " + cidade1 + " (" + estado1 + ") com " + cidade2 + " (" + estado2 + ")";
+    }
 }
