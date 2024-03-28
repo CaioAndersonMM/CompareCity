15b4497e1281bb0b896cc880adae982e0745323e
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-28 11:43:27 -0300

    Configuração do RestTemplate

---
+++ compare_city/src/main/java/br/edu/ufersa/compare_city/config/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package br.edu.ufersa.compare_city.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
