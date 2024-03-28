50468e299048685b5b39335de8b87ff983cdf705
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-03-28 11:44:06 -0300

    Novo mapeamento adicionado, o HTML deverá abrir com o MODEL que será a LIST de Cidades.
    Carece de correção!

--- compare_city/src/main/java/br/edu/ufersa/compare_city/HomePage.java
+++ compare_city/src/main/java/br/edu/ufersa/compare_city/HomePage.java
@@ -1,13 +1,30 @@
 package br.edu.ufersa.compare_city;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+import br.edu.ufersa.compare_city.cidade.Cidade;
+
+import org.springframework.ui.Model;
+
 
 @Controller
 public class HomePage {
-    
     @GetMapping("/")
     public String home() {
         return "index"; // Retorna o nome do arquivo HTML sem a extensão
+    }
+
+    @PostMapping("/cidades/comparacao")
+    public String compararCidades(@RequestBody List<Cidade> listaCidades, Model model) {
+        // Aqui você pode processar a lista de cidades como desejar
+        model.addAttribute("cidades", listaCidades);
+        return "comparador"; // Retorna o nome do arquivo HTML sem a extensão
     }
 }
