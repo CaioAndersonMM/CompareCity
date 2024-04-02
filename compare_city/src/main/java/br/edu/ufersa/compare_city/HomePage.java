c5e90701fc9056197efe73a722babf41e77170b1
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-04-02 19:57:56 -0300

    Usando algumas Collections

--- compare_city/src/main/java/br/edu/ufersa/compare_city/HomePage.java
+++ compare_city/src/main/java/br/edu/ufersa/compare_city/HomePage.java
@@ -1,26 +1,25 @@
 package br.edu.ufersa.compare_city;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.TreeMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 import br.edu.ufersa.compare_city.cidade.Cidade;
 import br.edu.ufersa.compare_city.cidade.CidadeRepository;
 
-
 import org.springframework.ui.Model;
-
 
 @Controller
 public class HomePage {
 
     private final CidadeRepository cidadeRepository = new CidadeRepository();
-
 
     @GetMapping("/")
     public String home() {
@@ -31,7 +30,42 @@ public class HomePage {
     public String compararCidades(Model model) {
         List<Cidade> listaCidades = cidadeRepository.extrairCidade();
         System.out.println(listaCidades);
+
+        LinkedList<LinkedList<Double>> dadosEconomicosPorCidade = new LinkedList<>();
+        Queue<Integer> dadosPopulacionais = new LinkedList<>();
+        ArrayList<Double[]> dadosMeioAmbientePorCidade = new ArrayList<>();
+        TreeMap<String, String> dadosBiomaESistemaCosteiro = new TreeMap<>();
+
+        for (Cidade cidade : listaCidades) {
+            LinkedList<Double> dadosEconomicos = new LinkedList<>();
+            dadosEconomicos.add(cidade.getPibPerCapta());
+            dadosEconomicos.add(cidade.getPercentualReceitasExternas());
+            dadosEconomicos.add(cidade.getIdh());
+            dadosEconomicos.add(cidade.getReceitasRealizadas());
+            dadosEconomicos.add(cidade.getDespesasEmpenhadas());
+            dadosEconomicosPorCidade.add(dadosEconomicos);
+
+            dadosPopulacionais.add(cidade.getPopulacao());
+            dadosPopulacionais.add(cidade.getDensidadeDemografica());
+
+            Double[] dadosMeioAmbiente = {
+                    cidade.getAreaUrbanizada(),
+                    cidade.getEsgotamentoSanitario(),
+                    cidade.getPercentualArborizacao(),
+                    cidade.getPercentualUrbanizacaoViasPublicas(),
+                    (double) cidade.getPopulacaoExpostaRisco()
+            };
+
+            dadosMeioAmbientePorCidade.add(dadosMeioAmbiente);
+            dadosBiomaESistemaCosteiro.put(cidade.getBioma(), cidade.getSistemaCosteiroMarinho());
+
+        }
         model.addAttribute("cidades", listaCidades);
+        model.addAttribute("dadosEconomicos", dadosEconomicosPorCidade);
+        model.addAttribute("dadosPopulacionais", dadosPopulacionais);
+        model.addAttribute("dadosMeioAmbiente", dadosMeioAmbientePorCidade);
+        model.addAttribute("dadosBiomaESistemaCosteiro", dadosBiomaESistemaCosteiro);
+
         return "comparador";
     }
 }
